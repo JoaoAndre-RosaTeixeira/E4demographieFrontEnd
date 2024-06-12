b1811eb58a65567d9e3a74aac16881f2d84feaf2
JoaoAndre-RosaTeixeira <91742556+JoaoAndre-RosaTeixeira@users.noreply.github.com>
2024-06-12 14:45:03 +0200

    fix accuracy

--- src/components/PredictionResult.vue
+++ src/components/PredictionResult.vue
@@ -6,6 +6,12 @@
     <p>Population Prédite: {{ result.predicted_population }}</p>
     <p>Année Cible: {{ result.target_year }}</p>
     <p>Accuracy:</p>
+    <li v-for="(item, index) in result.accuracy" :key="index">
+      <h1>{{index}}</h1>
+      <p>mae : {{ item.mae }}</p>
+      <p>r2 : {{ item.r2 }}</p>
+      <p>rmse : {{ item.rmse }}</p>
+    </li>
     <AccuracyProgressBar :accuracy="result.accuracy" />
     <div v-if="result.plot_url">
       <img :src="result.plot_url" alt="Population Forecast Plot" />
